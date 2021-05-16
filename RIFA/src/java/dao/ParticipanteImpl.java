68a1e1730af902db7cfd06fc354cf6146f8bc15f
Maximo Huaman Allccahuaman <mrenato.huaman@vallegrande.edu.pe>
2021-05-15 19:54:02 -0500

    Avanze del reto 03, dao, participante, conexión e interfaz ICRUD

---
+++ RIFA/src/java/dao/ParticipanteImpl.java
@@ -0,0 +1,88 @@
+package dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Participante;
+
+public class ParticipanteImpl extends Conexion implements ICRUD<Participante> {
+
+    @Override
+    public void registrar(Participante par) throws Exception {
+        String SQL = "INSERT INTO PARTICIPANTE (NOMPAR, APEPAR, CELPAR, ESTPAR) VALUES (?,?,?,?)";
+        try {
+            PreparedStatement ps = this.conectar().prepareStatement(SQL);
+            ps.setString(1, par.getNombre());
+            ps.setString(2, par.getApellido());
+            ps.setString(3, par.getCelular());
+            ps.setString(4, par.getEstado());
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("error en registrar en participante" + e);
+        }
+
+    }
+
+    @Override
+    public void modificar(Participante par) throws Exception {
+        String SQL = "UPDATE PARTICIPANTE SET (NOMPAR=?, APEPAR=?, CELPAR=?) WHERE IDPAR = ?";
+        try {
+            PreparedStatement ps = this.conectar().prepareStatement(SQL);
+            ps.setString(1, par.getNombre());
+            ps.setString(2, par.getApellido());
+            ps.setString(3, par.getCelular());
+            ps.setString(4, par.getEstado());
+            ps.setInt(5, par.getIde());
+            ps.executeUpdate();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("error en modificar Participante" + e);
+        }
+    }
+
+    @Override
+    public void eliminar(Participante par) throws Exception {
+        String SQL = "UPDATE PARTICIPANTE SET ESTPAR='I' WHERE IDPAR=?";
+        try {
+            PreparedStatement ps = this.conectar().prepareStatement(SQL);
+            ps.setInt(1, par.getIde());
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("error en eliminar Participante" + e);
+        }
+
+    }
+
+    @Override
+    public List<Participante> listarTodos() throws Exception {
+        List<Participante> listado = null;
+        Participante pars;
+        String SQL = "SELECT * FROM PARTICIPANTE";
+        try {
+            listado = new ArrayList();
+            Statement st = this.conectar().createStatement();
+            ResultSet rs = st.executeQuery(SQL);
+            while (rs.next()) {
+                pars = new Participante();
+                pars.setIde(rs.getInt("IDPAR"));
+                pars.setNombre(rs.getString("NOMPAR"));
+                pars.setApellido(rs.getString("APEPAR"));
+                pars.setCelular(rs.getString("CELPAR"));
+                pars.setEstado(rs.getString("ESTPAR"));
+                listado.add(pars);
+            }
+            rs.close();
+            st.close();
+        } catch (Exception e) {
+            System.out.println("error en lista Participante" + e);
+        } finally {
+            this.cerrar();
+        }
+        return listado;
+    }
+
+}
---
+++ RIFA/src/java/modelo/Participante.java
@@ -0,0 +1,54 @@
+
+package modelo;
+
+
+public class Participante {
+    
+    private int ide;
+    private String nombre;
+    private String apellido;
+    private String celular;
+    private String estado;
+
+    public int getIde() {
+        return ide;
+    }
+
+    public void setIde(int ide) {
+        this.ide = ide;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+}

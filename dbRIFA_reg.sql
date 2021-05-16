893112bcf08ec87df6b5e2e3fbdd13f962f728a6
Martín Marcelo Torres <martin.marcelo@vallegrande.edu.pe>
2021-05-15 21:14:41 -0500

    Registro Actualizado

--- dbRIFA_reg.sql
+++ dbRIFA_reg.sql
@@ -1,4 +1,11 @@
---Datos: PREMIOInsert Into PREMIO(CODPRE , DESPRE)Values('1','Canasta de Víveres - Box de Star Wars'),('2','Mouse Inalámbrico - MI Smart Band 6 XIAOMI'),('3','USB 60gb de Madera - Audífono Blueetooh + HDMI'),('4','Pqte. Papel Higiénico + 2 Bolzas de Arroz - Pie de Manzana'),('5','Juego de Platos - Set de Copas'),
-('6','Keke + Budín 1kg c/u - 4 Tintas para Impresora')
+--Datos: PREMIO
+Insert Into PREMIO
+(CODPRE , DESPRE)
+Values
+('1','Canasta de Viveres - Box de Star Wars'),
+('2','Mouse Inalambrico - MI Smart Band 6 XIAOMI'),
+('3','USB 60gb de Madera - Audifono Blueetooh + HDMI'),
+('4','Pqte. Papel Higienico + 2 Bolzas de Arroz - Pie de Manzana'),
+('5','Juego de Platos - Set de Copas'),
+('6','Keke + Budin 1kg c/u - 4 Tintas para Impresora')
 GO
-

adab0420125b0a7506951273cc361f2f021a3a63
Martín Marcelo Torres <martin.marcelo@vallegrande.edu.pe>
2021-05-19 21:22:46 -0500

    Registro Actualizado

--- dbRIFA_reg.sql
+++ dbRIFA_reg.sql
@@ -9,3 +9,25 @@ Values
 ('5','Juego de Platos - Set de Copas'),
 ('6','Keke + Budin 1kg c/u - 4 Tintas para Impresora')
 GO
+
+--Datos: PARTICIPANTE
+Insert Into PARTICIPANTE
+(NOMPAR,APEPAR,CELPAR,ESTPAR)
+Values
+('Jorge','Juensalida Rodriguez','956823411','A'),
+('Felipe','Sancho Sanchez','912200145','A'),
+('Jean Pool','Escate Soto','914502245','A'),
+('Julia','Cardenas Ruiz','945587746','A'),
+('Vanesa','Candela del Valle','945623105','A'),
+('Alex','Huaman Salcedo','999652148','A'),
+('Angel','Ascencio Campos','985664231','A'),
+('Angela','Abregu Vicente','911203546','A'),
+('Adriana','Yactayo Palacios','995422876','A'),
+('Natalia','Soto Mosquera','911452006','A'),
+('Alexia','Alacala Benavente','983302215','A'),
+('Alexandra','Seminario Cardenas','945510030','A'),
+('Fernando','Leguia Suyo','978445600','A'),
+('Juan','Juensalida Rodriguez','956823411','A'),
+('Anahi','Subauste Avalos','941250090','A')
+GO
+
